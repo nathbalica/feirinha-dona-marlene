c34041abf3ff294ee12245d030378345877f8995
nbalica <nbalica@alelo.com.br>
2023-09-29 08:15:50 -0300

    add app

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-import express, { json, Request, Response } from "express";
+import app from "./app";
 import dotenv from "dotenv";
 
-import fruitsRouter from "./routers/fruits-router";
-
 dotenv.config();
-
-const app = express();
-app.use(json());
-
-app.get("/health", (req: Request, res: Response) => res.send("ok!"));
-app.use(fruitsRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 });
+
+
